0208477d463f0b05b9b6458e60073c1963d49ea8
Gwan Thanakrit <thanakrit.j@thoughtworks.com>
2019-09-26 11:14:58 +0700

    [SETUP] Add dependencies for Mockito

---
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,32 @@
+package com.twu.biblioteca;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+
+public class BibliotecaAppTest {
+    private BibliotecaApp app;
+
+    private PrintStream printStream;
+    private BufferedReader bufferedReader;
+
+    @Before
+    public void setUp() throws Exception {
+        printStream = mock(PrintStream.class);
+        bufferedReader = mock(BufferedReader.class);
+    }
+
+    @Test
+    public void WhenStartTheAppItShouldSayHello() {
+
+        app.main();
+
+        verify(this.printStream).println("Welcome to Biblioteca. Your one-stop-shop for great books in Bangalore!");
+    }
+}
