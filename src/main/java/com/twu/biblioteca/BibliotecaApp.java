2145c2b1f359fb54d88686f4fd31267a8a1087d3
Gwan Thanakrit <thanakrit.j@thoughtworks.com>
2019-09-26 14:35:49 +0700

    [1.2#REFACTOR] Add main program to be able to use Library class

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,21 @@
 package com.twu.biblioteca;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
     public static void main(String[] args) {
-        
+        PrintStream printStream = new PrintStream(System.out);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<String> books = new ArrayList<>();
+        Library library = new Library(books, printStream, bufferedReader);
+
+        library.welcome();
+        library.listBooks();
     }
 }
