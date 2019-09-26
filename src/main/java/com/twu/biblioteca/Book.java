e31bf497a2e603f5d1a61dfaf9383320678e953c
Gwan Thanakrit <thanakrit.j@thoughtworks.com>
2019-09-26 15:40:37 +0700

    [1.3#IMPLEMENT] Add 3 methods for Book class about getting information

--- src/main/java/com/twu/biblioteca/Book.java
+++ src/main/java/com/twu/biblioteca/Book.java
@@ -1,4 +1,26 @@
 package com.twu.biblioteca;
 
 public class Book {
+    private String name;
+    private String author;
+    private int publishedYear;
+
+    public Book(String name, String author, int publishedYear){
+        this.name = name;
+        this.author = author;
+        this.publishedYear = publishedYear;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public int getPublishedYear(){
+        return publishedYear;
+    }
+
 }
