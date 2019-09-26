6873f8dbe0cbfd24428fc24cc1ee5cf4da91bb8e
Gwan Thanakrit <thanakrit.j@thoughtworks.com>
2019-09-26 15:39:54 +0700

    [1.3#TEST] Add test of Book Class

--- src/test/java/com/twu/biblioteca/BookTest.java
+++ src/test/java/com/twu/biblioteca/BookTest.java
@@ -1,4 +1,17 @@
 package com.twu.biblioteca;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class BookTest {
+    @Test
+    public void ShouldReturnNameWhenGetName(){
+        Book book = new Book("BookName1", "AuthorName1", 1997);
+
+        assertThat("BookName1", is(book.getName()));
+        assertThat("AuthorName1", is(book.getAuthor()));
+        assertThat(1997, is(book.getPublishedYear()));
+    }
 }
