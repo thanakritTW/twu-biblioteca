899083c9a21d8553883488fad3185534779924fd
Gwan Thanakrit <thanakrit.j@thoughtworks.com>
2019-10-08 09:56:35 +0700

    [2.2TEST] Add test on creating Movie

---
+++ src/test/java/com/twu/biblioteca/MovieTest.java
@@ -0,0 +1,27 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MovieTest {
+    @Test
+    public void ShouldReturnNameWhenGetName(){
+        Movie movie = new Movie("BookName1", "AuthorName1", 1997, 10);
+
+        assertThat("BookName1", is(movie.getName()));
+        assertThat("AuthorName1", is(movie.getDirector()));
+        assertThat(1997, is(movie.getYear()));
+    }
+
+    @Test
+    public void isAvailable_ShouldReturnTrue_RightAfterIniting(){
+        Movie movie = new Movie("BookName1", "AuthorName1", 1997, 10);
+
+        boolean expected = movie.isAvailable();
+        
+        assertThat(expected, is(true));
+    }
+
+}
